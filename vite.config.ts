42a2ad4837622152cf4b8cac18fdb9f926adb9fc
goose956 <132108181+goose956@users.noreply.github.com>
2025-06-05 09:37:47 +0100

    Update celtic/vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,19 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-    minify: 'esbuild'
-  },
-  server: {
-    port: 3000,
-    host: true
-  },
-  resolve: {
-    alias: {
-      '@': '/src'
-    }
-  }
 })
